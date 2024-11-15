aa3e06b7efe973e8d67da71efe785a535c06ba1e
P41T0 <tortmasoliver@gmail.com>
2024-11-15 12:54:16 +0100

    afegida la bala

---
+++ Assets/Scripts/BulletScript.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private float speed;
+    private Rigidbody rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        speed = 25f;
+        rb.velocity = Vector3.forward * speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/GunScript.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class GunScript : MonoBehaviour
+{
+    [SerializeField] private GameObject BulletSpawner;
+    [SerializeField] private GameObject bullet;
+    private float delayTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        delayTime = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (delayTime >= 0)
+        {
+            delayTime -= Time.deltaTime;
+        }
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Instantiate(bullet, BulletSpawner.transform.position, BulletSpawner.transform.rotation);
+            }
+        }
+
+    }
+}
